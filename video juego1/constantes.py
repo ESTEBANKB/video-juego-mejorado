0885436c976a1afb51239606a4207c2961b67529
ESTEBANKB <estebankb8@gmail.com>
2025-03-13 12:33:24 -0500

    Add files via upload
    este código hace que el personaje camine y se voltee hacia la derecha y izquierda

---
+++ video juego1/constantes.py
@@ -0,0 +1,17 @@
+# constantes
+# Tamaño de la ventana
+ANCHO_VENTANA = 800  
+ALTO_VENTANA = 600   
+
+# Definición de colores
+VERDE = (0, 200, 0)  # Color de fondo del juego
+
+# FPS del juego
+FPS = 60
+
+# Tamaño del personaje
+ANCHO_PERSONAJE = 70  
+ALTO_PERSONAJE = 70  
+
+# Velocidad del personaje
+VELOCIDAD_PERSONAJE = 5  
