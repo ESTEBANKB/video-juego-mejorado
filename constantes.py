236f682c85c373832c29afaa55c4c831b0ac317d
ESTEBANKB <estebankb8@gmail.com>
2025-03-18 00:01:03 -0500

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,26 @@
+# constantes
+# Tamaño de la ventana
+ANCHO_VENTANA = 800  
+ALTO_VENTANA = 600   
+
+
+# Definición de colores
+VERDE = (0, 200, 0)  # Color de fondo del juego
+# Color a eliminar (negro en este caso)
+COLOR_TRANSPARENTE = (0, 0, 0) 
+# FPS del juego
+BLANCO= (255, 255, 255)
+FPS = 60
+
+# Tamaño del personaje
+ANCHO_PERSONAJE = 45  
+ALTO_PERSONAJE = 45  
+
+# Tamaño de la moneda
+ANCHO_MONEDA = 50
+ALTO_MONEDA = 50
+#tamaño de la cuadricula
+CUADRICULA_TAMAÑO = 50
+TILE_TYPES= 30
+# Velocidad del personaje
+VELOCIDAD_PERSONAJE = 5  
